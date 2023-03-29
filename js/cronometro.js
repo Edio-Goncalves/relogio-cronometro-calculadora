2ae4a21f32916efa86da58717070306bd5301a6b
Edio Goncalves <ediogoncalvesjr@hotmail.com>
2023-03-28 21:07:09 -0300

    Refactor the stopwatch java script

--- js/cronometro.js
+++ js/cronometro.js
@@ -1,76 +1,77 @@
-const startBtn = document.querySelector("[data-start]");
-const pauseBtn = document.querySelector("[data-pause]");
-const stopBtn = document.querySelector("[data-stop]");
-const timeEl = document.querySelector("[data-time]");
+const minutesEl = document.querySelector("#minutess");
+const secondsEl = document.querySelector("#secondss");
+const millisecondsEl = document.querySelector("#millisecondss");
+const startBtn = document.querySelector("#startBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+const resumeBtn = document.querySelector("#resumeBtn");
+const resetBtn = document.querySelector("#resetBtn");
 
 let interval;
-let sec = 0,
-  min = 0,
-  hr = 0;
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+let isPaused = false;
 
-const renderTimer = (hr, min, sec) => {
-  const hrValue = hr < 10 ? "0" + hr : hr;
-  const minlue = min < 10 ? "0" + min : min;
-  const secValue = sec < 10 ? "0" + sec : sec;
+function startTimer() {
+  interval = setInterval(() => {
+    if (!isPaused) {
+      milliseconds += 10;
 
-  timeEl.innerHTML = hrValue + ":" + minlue + ":" + secValue;
-};
-function startTimer(startValue) {
-  startBtn.setAttribute("disabled", "true");
-  startBtn.innerHTML = "Start";
-  pauseBtn.removeAttribute("disabled");
-  stopBtn.removeAttribute("disabled");
-  if (startValue === "start" || startValue === "restart") {
-    (hr = 0), (min = 0), (sec = sec != 0 ? -1 : 0);
-    interval = setInterval(() => {
-      sec++;
-      while (sec === 60) {
-        sec = 0;
-        min++;
+      if (milliseconds === 1000) {
+        seconds++;
+        milliseconds = 0;
       }
-      while (min === 60) {
-        min = 0;
-        hr++;
+
+      if (seconds === 60) {
+        minutes++;
+        seconds = 0;
       }
-      renderTimer(hr, min, sec);
-    }, 1000);
-  } else if (startValue === "continue") {
-    interval = setInterval(() => {
-      sec++;
-      while (sec === 60) {
-        sec = 0;
-        min++;
-      }
-      while (min === 60) {
-        min = 0;
-        hr++;
-      }
-      renderTimer(hr, min, sec);
-    }, 1000);
-  }
+      millisecondsEl.textContent = formatMilleseconds(milliseconds);
+      secondsEl.textContent = formatTimer(seconds);
+      minutesEl.textContent = formatTimer(minutes);
+    }
+  }, 10);
+
+  startBtn.style.display = "none";
+  pauseBtn.style.display = "block";
 }
-const pauseTimer = () => {
-  startBtn.removeAttribute("disabled");
-  startBtn.setAttribute("data-start", "continue");
-  startBtn.innerHTML = "Continuar";
-  pauseBtn.setAttribute("disabled", "true");
+
+function formatTimer(time) {
+  return time < 10 ? "0" + time : time;
+}
+
+function formatMilleseconds(time) {
+  return time < 100 ? `${time}`.padStart(3, "0") : time;
+} /* padStart uma função usada com string, e time recebe numero, devemos transformar time em string `${}`*/
+
+function pauseTimer() {
+  isPaused = true;
+  pauseBtn.style.display = "none";
+  resumeBtn.style.display = "block";
+}
+
+function resumeTimer() {
+  isPaused = false;
+  pauseBtn.style.display = "block";
+  resumeBtn.style.display = "none";
+}
+
+function resetTimer() {
   clearInterval(interval);
-};
-const stopTime = () => {
-  startBtn.removeAttribute("disabled");
-  startBtn.setAttribute("data-start", "restart");
-  pauseBtn.setAttribute("disabled", "true");
-  stopBtn.setAttribute("disabled", "true");
-  startBtn.innerHTML = "Reiniciar";
-  clearInterval(interval);
-};
-startBtn.addEventListener("click", () => {
-  const startValue = startBtn.getAttribute("data-start");
-  startTimer(startValue);
-});
-pauseBtn.addEventListener("click", () => {
-  pauseTimer();
-});
-stopBtn.addEventListener("click", () => {
-  stopTime();
-});
+  minutes = 0;
+  seconds = 0;
+  milliseconds = 0;
+
+  minutesEl.textContent = "00";
+  secondsEl.textContent = "00";
+  millisecondsEl.textContent = "000";
+
+  pauseBtn.style.display = "none";
+  resumeBtn.style.display = "none";
+  startBtn.style.display = "block";
+}
+
+startBtn.addEventListener("click", startTimer);
+pauseBtn.addEventListener("click", pauseTimer);
+resumeBtn.addEventListener("click", resumeTimer);
+resetBtn.addEventListener("click", resetTimer);
